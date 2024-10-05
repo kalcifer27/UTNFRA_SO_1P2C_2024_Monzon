43e6ab9134d82d357ffb653e43243698fe6d58c8
JMonzon <monzonalanjoel@gmail.com>
2024-10-05 16:20:12 +0000

    ADD: formateo las particiones y las monto de forma persistente

--- RTA_SCRIPT_Examen_20241005/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241005/Punto_B.sh
@@ -2,7 +2,6 @@
 
 DISCO="/dev/sdc"
 
-echo "particionamos el disco: $DISCO"
 
 sudo fdisk $DISCO << EOF
 n
@@ -43,6 +42,35 @@ n
 
 w
 EOF
+
+echo
+sudo mkfs.ext4 ${DISCO}5
+sudo mkfs.ext4 ${DISCO}6
+sudo mkfs.ext4 ${DISCO}7
+sudo mkfs.ext4 ${DISCO}8
+sudo mkfs.ext4 ${DISCO}9
+sudo mkfs.ext4 ${DISCO}10
+sudo mkfs.ext4 ${DISCO}11
+sudo mkfs.ext4 ${DISCO}12
+sudo mkfs.ext4 ${DISCO}13
+sudo mkfs.ext4 ${DISCO}14
+
+echo
+
+echo "${DISCO}5 /Examenes-UTN/alumno_1/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}6 /Examenes-UTN/alumno_1/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}7 /Examenes-UTN/alumno_1/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}8 /Examenes-UTN/alumno_2/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}9 /Examenes-UTN/alumno_2/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}10 /Examenes-UTN/alumno_2/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}11 /Examenes-UTN/alumno_3/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}12 /Examenes-UTN/alumno_3/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}13 /Examenes-UTN/alumno_3/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}14 /Examenes-UTN/profesores ext4 defaults 0 0" | sudo tee -a /etc/fstab
+
+
+sudo mount -a 
+
 
 echo"resultado de la particion"
 sudo fdisk -l $DISCO
