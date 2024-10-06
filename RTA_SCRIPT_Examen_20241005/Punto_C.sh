6a01dbc94574ee244dde7e678f17021eff3eb832
JMonzon <monzonalanjoel@gmail.com>
2024-10-06 14:16:25 +0000

    FEAT: cambio permisos de direcotorios y sus dueños en Punto_C.sh

--- RTA_SCRIPT_Examen_20241005/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241005/Punto_C.sh
@@ -12,3 +12,16 @@ sudo useradd -m -p "$HASH" -s /bin/bash -G p1c2_2024_gAlumno p1c2_2024_A2
 sudo useradd -m -p "$HASH" -s /bin/bash -G p1c2_2024_gAlumno p1c2_2024_A3
 sudo useradd -m -p "$HASH" -s /bin/bash -G p1c2_2024_gProfesores p1c2_2024_P1
 
+echo
+echo "modifico los permisos de los directorios"
+sudo chmod -R 750 /Examenes-UTN/alumno_1
+sudo chmod -R 760 /Examenes-UTN/alumno_2
+sudo chmod -R 700 /Examenes-UTN/alumno_3
+sudo chmod -R 775 /Examenes-UTN/profesores
+echo
+
+echo "modifico dueños de los directorios"
+sudo chown -R p1c2_2024_A1:p1c2_2024_A1 /Examenes-UTN/alumno_1
+sudo chown -R p1c2_2024_A2:p1c2_2024_A2 /Examenes-UTN/alumno_2
+sudo chown -R p1c2_2024_A3:p1c2_2024_A3 /Examenes-UTN/alumno_3
+sudo chown -R p1c2_2024_P1:p1c2_2024_gProfesores /Examenes-UTN/profesores
