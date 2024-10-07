43afff81532b45c85d3bad891f250599a16fdedd
JMonzon <monzonalanjoel@gmail.com>
2024-10-07 02:09:03 +0000

    agrego archivo Filtro_Basico.txt y escribo script en Punto_E.sh

--- RTA_SCRIPT_Examen_20241005/Punto_E.sh
+++ RTA_SCRIPT_Examen_20241005/Punto_E.sh
@@ -6,3 +6,6 @@ echo
 echo "Filtro información memoria total"
 cd /repogit/UTNFRA_SO_1P2C_2024_Monzon/RTA_ARCHIVOS_Examen_20241005
 cat /proc/meminfo | grep -i "memtotal" > Filtro_basico.txt
+echo
+echo "Filtro información del fabricante del chasis"
+sudo dmidecode -t chassis | grep -i "manufacturer" >> Filtro_basico.txt
