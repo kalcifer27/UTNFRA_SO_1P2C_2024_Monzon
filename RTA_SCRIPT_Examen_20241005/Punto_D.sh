fba26a1f35559c9f79b132c1cab45a8510da408b
JMonzon <monzonalanjoel@gmail.com>
2024-10-07 01:37:47 +0000

    ADD: creo estructura asimétrica en Punto_D.sh

---
+++ RTA_SCRIPT_Examen_20241005/Punto_D.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+
+echo "creo los directorios"
+mkdir -p Estructura_Asimetrica/{correo,clientes}/{cartas_{1..100}} Estructura_Asimetrica/correo/carteros_{1..10}
+echo
+echo "muestro la estructura"
+tree Estructura_Asimetrica/ --noreport | pr -T -s' ' -w 80 --column 4
