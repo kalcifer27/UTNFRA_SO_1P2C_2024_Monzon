c63f90f55def2b58ac2e433cc58ce66d8861b12f
JMonzon <monzonalanjoel@gmail.com>
2024-10-10 22:35:50 +0000

    Fix: corrección Punto D

--- RTA_SCRIPT_Examen_20241005/Punto_D.sh
+++ RTA_SCRIPT_Examen_20241005/Punto_D.sh
@@ -1,8 +1,9 @@
 
 #!/bin/bash
 
+cd 
 echo "creo los directorios"
-mkdir -p Estructura_Asimetrica/{correo,cliente}/{cartas_{1..100}} Estructura_Asimetrica/correo/carteros_{1..10}
+mkdir -p Estructura_Asimetrica/{correo,cliente}/{cartas_{1..100}} Estructura_Asimetrica/correo/{carteros_{1..10}}
 echo
 echo "muestro la estructura"
 tree Estructura_Asimetrica/ --noreport | pr -T -s' ' -w 80 --column 4
